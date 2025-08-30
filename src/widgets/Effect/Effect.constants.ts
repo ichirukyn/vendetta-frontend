75fc516dbfca2fe5eaa3c8a25d1c4bfae52547e6
ichirukyn <ichiru.kyn@bk.ru>
2025-08-30 19:54:46 +0000

    fix: effect stats view

--- src/widgets/Effect/Effect.constants.ts
+++ src/widgets/Effect/Effect.constants.ts
@@ -38,16 +38,16 @@ export const EffectConstants = {
     { value: 'number', label: 'Добавить/Отнять число' },
     { value: 'percent', label: 'Добавить/Отнять процент' },
     { value: 'break', label: '', disabled: true },
-    
+
     { value: 'period', label: 'Периодический урон' },
     { value: 'control', label: 'Контроль противника' },
     { value: 'break_1', label: '', disabled: true },
-    
+
     { value: 'shield', label: 'Щит' },
     { value: 'heal', label: 'Хилл' },
     { value: 'dependency', label: 'Зависимость от статов' },
     { value: 'break_2', label: '', disabled: true },
-    
+
     { value: 'coast', label: 'Затраты' },
     { value: 'activate', label: 'Условия активации' },
     { value: 'hidden', label: 'Скрытый эффект' },
@@ -55,51 +55,50 @@ export const EffectConstants = {
   attribute: [
     { value: 'race_id', label: 'Раса (По id)' },
     { value: 'class_id', label: 'Класс (По id)' },
-    
+
     { value: 'mana_view', label: 'Мана:', disabled: true },
     { value: 'mana', label: 'Мана' },
     { value: 'mana_max', label: 'Мана от макс. значения' },
     { value: 'mana_percent', label: 'Процент маны' },
     { value: 'control_mana', label: 'Контроль маны' },
-    
+
     { value: 'hp_view', label: 'Хп:', disabled: true },
     { value: 'hp', label: 'Хп' },
     { value: 'hp_max', label: 'Хп от макс. значения' },
     { value: 'hp_percent', label: 'Процент Хп' },
-    
+
     { value: 'qi_view', label: 'Ки:', disabled: true },
     { value: 'qi', label: 'Ки' },
     { value: 'qi_max', label: 'Ки от макс. значения' },
     { value: 'qi_percent', label: 'Процент ки' },
     { value: 'control_qi', label: 'Контроль ки' },
-    
+
     { value: 'shield_view', label: 'Щит:', disabled: true },
     { value: 'shield', label: 'Щит' },
     { value: 'shield_max', label: 'Щит от макс. значения' },
     { value: 'shield_percent', label: 'Процент Щита' },
-    
-    { ...EffectStats, },
-    
-    { ...EffectFlatStats, },
-    
+
+    ...EffectStats,
+
+    ...EffectFlatStats,
+
     { value: 'crit_view', label: 'Криты:', disabled: true },
     { value: 'crit_damage', label: 'Крит урон' },
     { value: 'crit_rate', label: 'Крит. шанс попадания' },
-    
+
     { value: 'bonus_resist_view', label: 'Бонусы/Сопротивления:', disabled: true },
     { value: 'bonus_damage', label: 'Бонус урона' },
     { value: 'ignore_resist', label: 'Игнорирование сопротивления' },
     { value: 'resist', label: 'Сопротивление урону' },
     { value: 'weapon_damage', label: 'Урон оружия' },
-    
+
     { value: 'effect_view', label: 'Эффекты:', disabled: true },
     { value: 'effect_resist', label: 'Сопротивление эффектам' },
     { value: 'effect_chance', label: 'Шанс попадания эффектов' },
     // { value: 'debuff_resist', label: 'Сопротивление дебаффам' },
-    
-    
-    { ...ElementDamage },
-    
+
+    ...ElementDamage,
+
     { value: 'element_resist_view', label: 'Сопротивление стихий:', disabled: true },
     { value: 'fire_resist', label: 'Сопротивление к огню' },
     { value: 'water_resist', label: 'Сопротивление к воде' },
@@ -108,13 +107,13 @@ export const EffectConstants = {
     { value: 'light_resist', label: 'Сопротивление к свету' },
     { value: 'dark_resist', label: 'Сопротивление к тьме' },
     { value: 'phys_resist', label: 'Сопротивление к физ. урону' },
-    
+
     { value: 'prev_view', label: 'Разное:', disabled: true },
     { value: 'prev', label: 'Прошлое значение' },
     { value: 'prev_percent', label: 'Прошлый процент' },
     { value: 'total_stats', label: 'Всего ОС' },
     { value: 'lvl', label: 'Уровень' },
-    
+
     { value: 'modify_view', label: 'Модификаторы:', disabled: true },
     { value: 'mana_modify', label: 'Модификатор маны' },
     { value: 'hp_modify', label: 'Модификатор Хп' },
@@ -122,7 +121,6 @@ export const EffectConstants = {
     { value: 'evasion_modify', label: 'Модификатор Уклонения' },
     { value: 'counter_modify', label: 'Модификатор Контрудара' },
     { value: 'defence_modify', label: 'Модификатор Защиты' },
-  
   ],
   condition: [
     { value: '>', label: 'Больше' },
@@ -161,7 +159,7 @@ export const EffectConstants = {
     // { value: 'mana_max', label: 'Мана от макс. значения' },
     // { value: 'mana_percent', label: 'Процент маны' },
     // { value: 'control_mana', label: 'Контроль маны' },
-    
+
     // { value: 'qi_view', label: 'Ки:', disabled: true },
     { value: 'qi', label: 'Ки' },
     // { value: 'qi_max', label: 'Ки от макс. значения' },
